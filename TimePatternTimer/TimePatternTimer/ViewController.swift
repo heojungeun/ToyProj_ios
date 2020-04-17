9a739d9307259a3ee1c037f3150e3a3e69802647
heojungeun <heocube@naver.com>
2020-04-17 16:04:51 +0900

    init timepatterntimer

---
+++ TimePatternTimer/TimePatternTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimePatternTimer
+//
+//  Created by jung on 2020/04/17.
+//  Copyright © 2020 jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
