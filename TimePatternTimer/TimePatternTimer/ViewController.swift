a46cae6b747857b6849a1571bdf274cad509c6bd
heojungeun <heocube@naver.com>
2020-04-17 21:48:42 +0900

    add datepicker time label

--- TimePatternTimer/TimePatternTimer/ViewController.swift
+++ TimePatternTimer/TimePatternTimer/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var IblPickerTime: UILabel!
+    @IBOutlet var IblRestTime: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        
+        let datePickerView = sender // 전달 인수 저장
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        
+        IblPickerTime.text = "선택시간 : " + formatter.string(from: datePickerView.date)
+    }
 
+    @IBAction func startTimer(_ sender: Any) {
+        
+    }
+    
+    @IBAction func cancelTimer(_ sender: Any) {
+        
+    }
 }
 
