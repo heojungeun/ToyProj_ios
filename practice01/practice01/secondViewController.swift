40036a1b14b1e686df5378ea097e43e11436968d
heojungeun <heocube@naver.com>
2020-04-19 21:06:42 +0900

    add secviewcontroller, img set

---
+++ practice01/practice01/secondViewController.swift
@@ -0,0 +1,27 @@
+//
+//  secondViewController.swift
+//  practice01
+//
+//  Created by jung on 2020/04/19.
+//  Copyright © 2020 jung. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    @IBOutlet weak var imgview: UIImageView!
+    @IBOutlet weak var scalelbl: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func scalebtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func switchImgonoff(_ sender: UISwitch) {
+    }
+}
