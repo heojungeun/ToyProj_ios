747a92d35331953248353c975650493196a44756
heojungeun <heocube@naver.com>
2020-04-19 20:15:34 +0900

    init pj practice, modified storyboard, view controller

---
+++ practice01/practice01/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  practice01
+//
+//  Created by jung on 2020/04/19.
+//  Copyright © 2020 jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var firstlbl: UILabel!
+    @IBOutlet weak var seclbl: UILabel!
+    @IBOutlet weak var chgtxt: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func firstbtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func secbtn(_ sender: UIButton) {
+        
+    }
+    
+    
+}
+
