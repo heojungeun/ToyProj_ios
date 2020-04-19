2b8f31c3ded0a17e5dfe2e936588a913d85a2d7f
heojungeun <heocube@naver.com>
2020-04-19 21:57:25 +0900

    modified sec vc - btnresize, switchonoff

--- practice01/practice01/secondViewController.swift
+++ practice01/practice01/secondViewController.swift
@@ -13,15 +13,44 @@ class secondViewController: UIViewController {
     @IBOutlet weak var imgview: UIImageView!
     @IBOutlet weak var scalelbl: UIButton!
     
+    var isZoom = false
+    var imgOn: UIImage?
+    var imgOff: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imgOn = #imageLiteral(resourceName: "bulblight")
+        imgOff = #imageLiteral(resourceName: "bulbblack")
+        
+        imgview.image = imgOn
     }
     
     @IBAction func scalebtn(_ sender: UIButton) {
+        let scale: CGFloat = 2.0
+        var newWidth: CGFloat, newHeight: CGFloat
+        
+        if (isZoom){
+            // true 확대된 상태일때
+            newWidth = imgview.frame.width/scale
+            newHeight = imgview.frame.height/scale
+            imgview.frame.size = CGSize(width: newWidth, height: newHeight)
+            scalelbl.setTitle("확대", for: .normal)
+        }else{
+            //false
+            newWidth = imgview.frame.width*scale
+            newHeight = imgview.frame.height*scale
+            imgview.frame.size = CGSize(width: newWidth, height: newHeight)
+            scalelbl.setTitle("축소", for: .normal)
+        }
+        isZoom = !isZoom
     }
     
     @IBAction func switchImgonoff(_ sender: UISwitch) {
+        if sender.isOn {
+            imgview.image = imgOn
+        }else{
+            imgview.image = imgOff
+        }
     }
 }
