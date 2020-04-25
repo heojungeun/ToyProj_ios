3b2462738c9953a94226932394ab4f68c69da805
heojungeun <heocube@naver.com>
2020-04-25 21:37:26 +0900

    add rest timer (TODO: rest time fix after stop)

--- TimePatternTimer/TimePatternTimer/ViewController.swift
+++ TimePatternTimer/TimePatternTimer/ViewController.swift
@@ -10,15 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var alarmTime: Date?
+    var currentTime: Date?
+    
+    //timer
+    var mTimer : Timer?
+    
     @IBOutlet var IblPickerTime: UILabel!
     @IBOutlet var IblRestTime: UILabel!
     
-    private let step: Double = 1.0
-    private var timer: Timer?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        alarmTime = formatter.date(from: "00:01:00")
+        
     }
 
     @IBAction func changeDatePicker(_ sender: UIDatePicker) {
@@ -26,17 +35,40 @@ class ViewController: UIViewController {
         let datePickerView = sender // 전달 인수 저장
         
         let formatter = DateFormatter()
-        formatter.dateFormat = "HH:mm:ss"
-        
+        formatter.dateFormat = "hh:mm:ss"
         IblPickerTime.text = "선택시간 : " + formatter.string(from: datePickerView.date)
+        alarmTime = datePickerView.date
     }
 
     @IBAction func startTimer(_ sender: Any) {
         // start 버튼 눌렀을 때 시간 적용해서 타이머 시작하기
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
     
     @IBAction func cancelTimer(_ sender: Any) {
         // 취소 버튼 눌렀을 때 선택시간, 남은시간 초기화하기
+        if let timer = mTimer{
+            if(timer.isValid){
+                timer.invalidate()
+            }
+        }
+        
+        IblPickerTime.text = "선택 시간 : "
+        IblRestTime.text = "남은 시간 : "
+    }
+    
+    @objc
+    func updateTime(){
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        currentTime = formatter.date(from: "00:00:00")
+        alarmTime?.addTimeInterval(TimeInterval(-1.0))
+        IblRestTime.text = "남은 시간 : " + formatter.string(from: alarmTime!)
+        if (alarmTime == currentTime){
+            view.backgroundColor = UIColor.red
+        } else {
+            view.backgroundColor = UIColor.white
+        }
     }
     
 }
