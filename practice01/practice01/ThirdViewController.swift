7b5cfcb5fddd51c3e8944d6daef5a969879efb35
heojungeun <heocube@naver.com>
2020-04-25 20:22:29 +0900

    add alarm color

--- practice01/practice01/ThirdViewController.swift
+++ practice01/practice01/ThirdViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
+    let timeSelector: Selector = #selector(ThirdViewController.updateTime)
+    let interval = 1.0
+    var cnt = 0
+    var alarmTime: String?
     
     @IBOutlet weak var lblCurTime: UILabel!
     @IBOutlet weak var lblPickTime: UILabel!
@@ -17,7 +21,8 @@ class ThirdViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+        
     }
 
     @IBAction func changeDatePicker(_ sender: UIDatePicker) {
@@ -26,5 +31,26 @@ class ThirdViewController: UIViewController {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
         lblPickTime.text = formatter.string(from: datePickerView.date)
+        formatter.dateFormat = "hh:mm aaa"
+        alarmTime = formatter.string(from: datePickerView.date)
+    }
+    
+    @objc
+    func updateTime(){
+//        lblCurTime.text = String(cnt)
+//        cnt = cnt + 1
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblCurTime.text = formatter.string(from: date as Date)
+        
+        formatter.dateFormat = "hh:mm:ss aaa"
+        let chcurtime = formatter.string(from: date as Date)
+        if (alarmTime == chcurtime){
+            view.backgroundColor = UIColor.red
+        } else {
+            view.backgroundColor = UIColor.white
+        }
     }
 }
