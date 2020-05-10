ec50dc38d3ee5bc7427739732c34c5829f932593
heojungeun <heocube@naver.com>
2020-05-10 20:43:02 +0900

    init practice02

---
+++ practice02/practice02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practice02
+//
+//  Created by jung on 2020/05/10.
+//  Copyright © 2020 jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
