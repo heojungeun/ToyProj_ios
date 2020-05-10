fd3ff958a848adb1c6fcf5ee2cd8fcb26e805297
heojungeun <heocube@naver.com>
2020-05-10 21:18:42 +0900

    modified vc

--- practice02/practice02/ViewController.swift
+++ practice02/practice02/ViewController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate {
+    
+    let MAX_ARRAY_NUM = 10
+    let PICKER_VIEW_COLUMN = 1
+    var imageFileName = [
+        "m1.png", "m2.png" , "m3.png" , "m4.png" , "m5.png" , "m6.png"
+    ]
 
     @IBOutlet weak var pickerImage: UIPickerView!
     @IBOutlet weak var IblImageFileName: UILabel!
@@ -19,6 +25,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    func numberOfComponentsInPickerView(pickerView: UIPickerView!) -> Int {
+        return PICKER_VIEW_COLUMN
+    }
+    
+    func pickerView(_ pickerView: UIPickerView!, numberOfRowsInComponent component: Int) -> Int{
+        return imageFileName.count
+    }
 
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return imageFileName[row]
+    }
 }
 
