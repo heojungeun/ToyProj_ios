496614823e0d30aae8a009c60b96bd1a68b716b2
heojungeun <heocube@naver.com>
2020-09-13 23:08:47 +0900

    add string format, msec

--- firebasePush/firebasePush/ViewController.swift
+++ firebasePush/firebasePush/ViewController.swift
@@ -10,18 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var counter = 0
+    var counter = 0.0
     var timer = Timer()
     var isPlaying = false
     
-    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var minLbl: UILabel!
+    @IBOutlet weak var secLbl: UILabel!
+    @IBOutlet weak var msecLbl: UILabel!
     @IBOutlet weak var startBtn: UIButton!
     @IBOutlet weak var pauseBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        timeLabel.text = timeFormatter(counter)
+        ChangeLabel(timeFormatter(counter))
         pauseBtn.isEnabled = false
     }
 
@@ -32,7 +34,7 @@ class ViewController: UIViewController {
         startBtn.isEnabled = false
         pauseBtn.isEnabled = true
         
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
         isPlaying = true
     }
     
@@ -51,22 +53,35 @@ class ViewController: UIViewController {
         timer.invalidate()
         isPlaying = false
         counter = 0
-        timeLabel.text = timeFormatter(counter)
+        //timeLabel.text = timeFormatter(counter)
+        ChangeLabel(timeFormatter(counter))
     }
     
     @objc func UpdateTimer(){
-        counter = counter + 1
-        timeLabel.text = timeFormatter(counter)
+        counter = counter + 0.001
+        ChangeLabel(timeFormatter(counter))
     }
     
-    func timeFormatter(_ intTime: Int) -> String {
+    func ChangeLabel(_ timeStr: String){
+        let timeSplit = timeStr.split(separator: ":")
+        minLbl.text = String(timeSplit[0])
+        secLbl.text = String(timeSplit[1])
+        msecLbl.text = String(timeSplit[2])
+    }
+    
+    // 참고한 사이트 : http://yoonbumtae.com/?p=2310
+    func timeFormatter(_ intTimeinterval: TimeInterval) -> String {
+        let intTime = NSInteger(intTimeinterval)
+        
         let min = (intTime % 3600) / 60
         let sec = (intTime % 3600) % 60
+        let msec = Int((intTimeinterval.truncatingRemainder(dividingBy: 1)) * 1000) / 10
         
-        let minStr = min < 10 ? "0\(min)" : String(min)
-        let secStr = sec < 10 ? "0\(sec)" : String(sec)
+//        let minStr = min < 10 ? "0\(min)" : String(min)
+//        let secStr = sec < 10 ? "0\(sec)" : String(sec)
         
-        return "\(minStr):\(secStr)"
+        //return "\(minStr):\(secStr)"
+        return String(format: "%02d:%02d:%02d", min, sec, msec)
     }
     
 }
