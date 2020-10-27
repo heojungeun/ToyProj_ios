1acc75ed91f90107f28d9785520e3950442ca784
heojungeun <heocube@naver.com>
2020-10-27 23:18:31 +0900

    add page view controller

--- InstagramFeedcam/InstagramFeedcam/ViewController.swift
+++ InstagramFeedcam/InstagramFeedcam/ViewController.swift
@@ -4,6 +4,13 @@
 //
 //  Created by jung on 2020/10/22.
 //
+//  출처:https://github.com/allenwong/30DaysofSwift/blob/master/Project%2004%20-%20SnapChatMenu/Snapchat%20Menu/ViewController.swift
+//  ViewController.swift
+//  Snapchat Menu
+//
+//  Created by Allen on 16/1/10.
+//  Copyright © 2016年 Allen. All rights reserved.
+//
 
 import UIKit
 
@@ -14,6 +21,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    
 }
 
