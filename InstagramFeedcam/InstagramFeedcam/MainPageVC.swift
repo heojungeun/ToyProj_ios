e75584d76f29104cd465ad08a4fec02b0107e9be
heojungeun <heocube@naver.com>
2020-10-28 23:16:04 +0900

    초기 pageVC 완성

--- InstagramFeedcam/InstagramFeedcam/MainPageVC.swift
+++ InstagramFeedcam/InstagramFeedcam/MainPageVC.swift
@@ -21,32 +21,67 @@ class MainPageVC: UIPageViewController, UIPageViewControllerDataSource, UIPageVi
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        <#code#>
+        guard let viewControllerIndex = VCArray.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard VCArray.count > previousIndex else {
+            return nil
+        }
+        
+        return VCArray[previousIndex]
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        <#code#>
+        guard let viewControllerIndex = VCArray.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex < VCArray.count else {
+            return nil
+        }
+        
+        guard VCArray.count > nextIndex else {
+            return nil
+        }
+        
+        return VCArray[nextIndex]
+    }
+    
+    public func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return VCArray.count
+    }
+    
+    public func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        guard let firstViewController = viewControllers?.first, let firstViewControllerIndex = VCArray.firstIndex(of: firstViewController) else {
+            return 0
+        }
+        
+        return firstViewControllerIndex
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.dataSource = self
+        self.delegate = self
+        
+        if let firstVC = VCArray.first {
+            setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
