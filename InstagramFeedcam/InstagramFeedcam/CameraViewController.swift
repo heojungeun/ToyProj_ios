2013068244f962a1a8c6c18533aa42e7fe14f12e
heojungeun <heocube@naver.com>
2020-10-28 23:28:15 +0900

    초기 카메라 VC 세팅

---
+++ InstagramFeedcam/InstagramFeedcam/CameraViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CameraViewController.swift
+//  InstagramFeedcam
+//
+//  Created by jung on 2020/10/28.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
