e8b85cf09f2d57a64f5aac52da40cd7d6d0bab00
heojungeun <heocube@naver.com>
2020-10-29 23:20:07 +0900

    update cameraVC

--- InstagramFeedcam/InstagramFeedcam/CameraViewController.swift
+++ InstagramFeedcam/InstagramFeedcam/CameraViewController.swift
@@ -3,18 +3,91 @@
 //  InstagramFeedcam
 //
 //  Created by jung on 2020/10/28.
-//
+//  ref https://guides.codepath.com/ios/Creating-a-Custom-Camera-View
 
 import UIKit
 import AVFoundation
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
 
+    
+    @IBOutlet weak var preView: UIView!
+    @IBOutlet weak var captureImageView: UIImageView!
+    
+    var captureSession: AVCaptureSession!
+    var stillImageOutput: AVCapturePhotoOutput!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Do any additional setup after loading the view.
     }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .medium
+        
+        guard let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+        else {
+            print("Unable to access back camera!")
+            return
+        }
+        
+        do{
+            let input = try AVCaptureDeviceInput(device: backCamera)
+            
+            stillImageOutput = AVCapturePhotoOutput()
+            
+            if captureSession.canAddInput(input) && captureSession.canAddOutput(stillImageOutput){
+                captureSession.addInput(input)
+                captureSession.addOutput(stillImageOutput)
+                setupLivePreview()
+            }
+        }
+        catch let error {
+            print("Error Unable to initialize back camera: \(error.localizedDescription)")
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.captureSession.stopRunning()
+    }
+    
+    // 실시간 카메라 화면
+    func setupLivePreview(){
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        videoPreviewLayer.videoGravity = .resizeAspect
+        videoPreviewLayer.connection?.videoOrientation = .portrait
+        preView.layer.addSublayer(videoPreviewLayer)
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            // 백그라운드 스레드에서 세션 시작
+            self.captureSession.startRunning()
+            
+            DispatchQueue.main.async {
+                self.videoPreviewLayer.frame = self.preView.bounds
+            }
+        }
+        
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard let imageData = photo.fileDataRepresentation()
+        else { return }
+        
+        let image = UIImage(data: imageData)
+        captureImageView.image = image
+    }
+    
+    @IBAction func takePicture(_ sender: Any) {
+        let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
+        stillImageOutput.capturePhoto(with: settings, delegate: self)
+    }
+    
 }
