5a2af99940c2a816e81362822cf62d258449bd36
heojungeun <heocube@naver.com>
2020-11-09 00:25:01 +0900

    카메라 프리뷰 다시 시작

--- InstagramFeedcam/InstagramFeedcam/CameraViewController.swift
+++ InstagramFeedcam/InstagramFeedcam/CameraViewController.swift
@@ -2,92 +2,37 @@
 //  CameraViewController.swift
 //  InstagramFeedcam
 //
-//  Created by jung on 2020/10/28.
-//  ref https://guides.codepath.com/ios/Creating-a-Custom-Camera-View
+// ref. https://medium.com/@anuragajwani/how-to-process-images-real-time-from-the-ios-camera-9c416c531749
+
 
 import UIKit
 import AVFoundation
 
-class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate {
-
+class CameraViewController: UIViewController {
     
-    @IBOutlet weak var preView: UIView!
-    @IBOutlet weak var captureImageView: UIImageView!
-    
-    var captureSession: AVCaptureSession!
-    var stillImageOutput: AVCapturePhotoOutput!
-    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    private let captureSession = AVCaptureSession()
+    private lazy var previewLayer: AVCaptureVideoPreviewLayer = {
+        let preview = AVCaptureVideoPreviewLayer(session: self.captureSession)
+        preview.videoGravity = .resizeAspect
+        return preview
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
         // Do any additional setup after loading the view.
+        
+        self.addCameraInput()
+        self.addPreviewLayer()
+        self.captureSession.startRunning()
+    }
+
+    private func addCameraInput(){
+        let device = AVCaptureDevice.default(for: .video)!
+        let cameraInput = try! AVCaptureDeviceInput(device: device)
+        self.captureSession.addInput(cameraInput)
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        captureSession = AVCaptureSession()
-        captureSession.sessionPreset = .medium
-        
-        guard let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
-        else {
-            print("Unable to access back camera!")
-            return
-        }
-        
-        do{
-            let input = try AVCaptureDeviceInput(device: backCamera)
-            
-            stillImageOutput = AVCapturePhotoOutput()
-            
-            if captureSession.canAddInput(input) && captureSession.canAddOutput(stillImageOutput){
-                captureSession.addInput(input)
-                captureSession.addOutput(stillImageOutput)
-                setupLivePreview()
-            }
-        }
-        catch let error {
-            print("Error Unable to initialize back camera: \(error.localizedDescription)")
-        }
+    private func addPreviewLayer(){
+        self.view.layer.addSublayer(self.previewLayer)
     }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        self.captureSession.stopRunning()
-    }
-    
-    // 실시간 카메라 화면
-    func setupLivePreview(){
-        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        
-        videoPreviewLayer.videoGravity = .resizeAspect
-        videoPreviewLayer.connection?.videoOrientation = .portrait
-        preView.layer.addSublayer(videoPreviewLayer)
-        
-        DispatchQueue.global(qos: .userInitiated).async {
-            // 백그라운드 스레드에서 세션 시작
-            self.captureSession.startRunning()
-            
-            DispatchQueue.main.async {
-                self.videoPreviewLayer.frame = self.preView.bounds
-            }
-        }
-        
-    }
-    
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        guard let imageData = photo.fileDataRepresentation()
-        else { return }
-        
-        let image = UIImage(data: imageData)
-        captureImageView.image = image
-    }
-    
-    @IBAction func takePicture(_ sender: Any) {
-        let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
-        stillImageOutput.capturePhoto(with: settings, delegate: self)
-    }
-    
 }
